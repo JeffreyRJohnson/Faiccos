1e91c95857122ed15370706ee8c0562a9d609c1b
Jeff <jeffreyrjohnsonsemail@gmail.com>
2017-10-06 14:04:23 -0500

    styles added to build

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,8 +3,8 @@ var gulp = require('gulp'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
-    mixins = require('postcss-mixins'),
-    cssImport = require('postcss-import');
+    cssImport = require('postcss-import'),
+    mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
