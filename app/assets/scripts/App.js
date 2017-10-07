f9a3f60fdd3546b84a31dcfceb96876f8e79c360
Jeff <jeffreyrjohnsonsemail@gmail.com>
2017-10-07 12:58:43 -0500

    added modal functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".info__image__about"), "75%");
 new RevealOnScroll($(".info__image__reservations"), "100%");
 new RevealOnScroll($(".specials-reveal"), "60%");
+var modal = new Modal();
